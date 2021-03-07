184ceaa36555823f8aec10d7c3affb1618a48596
Britel Chaimaa <52888345+britel-chaimaa20@users.noreply.github.com>
2021-03-07 13:15:35 +0100

    Update 0-slice_me_up.py

--- 0x00-linear_algebra/0-slice_me_up.py
+++ 0x00-linear_algebra/0-slice_me_up.py
@@ -1,3 +1,4 @@
+alexa@ubuntu-xenial:0x00-linear_algebra$ cat 0-slice_me_up.py 
 #!/usr/bin/env python3
 arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
 arr1 = arr[0:2]
@@ -6,3 +7,10 @@ arr3 = arr[1:6]
 print("The first two numbers of the array are: {}".format(arr1))
 print("The last five numbers of the array are: {}".format(arr2))
 print("The 2nd through 6th numbers of the array are: {}".format(arr3))
+alexa@ubuntu-xenial:0x00-linear_algebra$ ./0-slice_me_up.py 
+The first two numbers of the array are: [9, 8]
+The last five numbers of the array are: [9, 4, 1, 0, 3]
+The 2nd through 6th numbers of the array are: [8, 2, 3, 9, 4]
+alexa@ubuntu-xenial:0x00-linear_algebra$ wc -l 0-slice_me_up.py 
+8 0-slice_me_up.py
+alexa@ubuntu-xenial:0x00-linear_algebra$ 
