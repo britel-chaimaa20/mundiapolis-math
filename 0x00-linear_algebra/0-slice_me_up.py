6dfc917d6f633b8bd0c93d57bd00b8fd54be3a73
Britel Chaimaa <52888345+britel-chaimaa20@users.noreply.github.com>
2021-03-07 23:41:16 +0100

    Update 0-slice_me_up.py

--- 0x00-linear_algebra/0-slice_me_up.py
+++ 0x00-linear_algebra/0-slice_me_up.py
@@ -6,3 +6,5 @@ arr3 = arr[1:6]  # your code here
 print("The first two numbers of the array are: {}".format(arr1))
 print("The last five numbers of the array are: {}".format(arr2))
 print("The 2nd through 6th numbers of the array are: {}".format(arr3))
+
+
