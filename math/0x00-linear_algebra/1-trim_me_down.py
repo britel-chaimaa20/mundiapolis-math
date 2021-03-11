23fd1bd1e7cc0d58b154b6c8cccd7c01d1526dfe
Britel Chaimaa <52888345+britel-chaimaa20@users.noreply.github.com>
2021-03-11 12:03:49 +0100

    Update 1-trim_me_down.py

--- math/0x00-linear_algebra/1-trim_me_down.py
+++ math/0x00-linear_algebra/1-trim_me_down.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
+matrix = [[1, 3, 9, 4, 5, 8],
+          [2, 4, 7, 3, 4, 0],
+          [0, 3, 4, 6, 1, 5]]
 the_middle = []
 for index in range(3):
     the_middle.append(matrix[index][2:4])
