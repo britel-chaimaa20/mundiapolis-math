ae3ce09a6dcf66940674f7835ce3050784b805e6
Britel Chaimaa <52888345+britel-chaimaa20@users.noreply.github.com>
2021-03-11 10:09:23 +0100

    Update 2-main.py

--- math/0x00-linear_algebra/2-main.py
+++ math/0x00-linear_algebra/2-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-
 matrix_shape = __import__('2-size_me_please').matrix_shape
+
 mat1 = [[1, 2], [3, 4]]
 print(matrix_shape(mat1))
 mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
