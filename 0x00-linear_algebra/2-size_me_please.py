10dcd4d2202baa9b5449ae1e9e43752eb488aa5d
Britel Chaimaa <52888345+britel-chaimaa20@users.noreply.github.com>
2021-03-11 11:41:57 +0100

    Delete 0x00-linear_algebra directory

--- 0x00-linear_algebra/2-size_me_please.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-
-def matrix_shape(matrix):
-    if type(matrix[0]) is not list:
-        return [len(matrix)]
-    else:
-        return [len(matrix)] + matrix_shape(matrix[0])
--- 0x00-linear_algebra/3-flip_me_over.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-
-def matrix_transpose(matrix):
-    try:
-        return [[matrix[j][i] for j in range(len(matrix))]
-                for i in range(len(matrix[0]))]
-    except (TypeError, IndexError):
-        return [[matrix[i]] for i in range(len(matrix))]
