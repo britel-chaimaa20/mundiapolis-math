b1ccdf2cd66c819f9ca75ab01a5892653ca733ac
Britel Chaimaa <52888345+britel-chaimaa20@users.noreply.github.com>
2021-03-11 10:47:14 +0100

    Update 2-size_me_please.py

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
-    if type(matrix[0]) is not list:
+    if type(matrix[0]) != list:
         return [len(matrix)]
     else:
         return [len(matrix)] + matrix_shape(matrix[0])
