fefc53cbfa9db38f3786e4d88acc5b6044cb4866
Britel Chaimaa <52888345+britel-chaimaa20@users.noreply.github.com>
2021-03-12 17:15:18 +0100

    Update 9-let_the_butcher_slice_it.py

--- math/0x00-linear_algebra/9-let_the_butcher_slice_it.py
+++ math/0x00-linear_algebra/9-let_the_butcher_slice_it.py
@@ -1,4 +1,3 @@
-alexa@ubuntu-xenial:0x00-linear_algebra$ cat 9-let_the_butcher_slice_it.py 
 #!/usr/bin/env python3
 import numpy as np
 matrix = np.array([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12],
@@ -9,19 +8,3 @@ mat3 = matrix[1:, 3:6]
 print("The middle two rows of the matrix are:\n{}".format(mat1))
 print("The middle two columns of the matrix are:\n{}".format(mat2))
 print("The bottom-right, square, 3x3 matrix is:\n{}".format(mat3))
-alexa@ubuntu-xenial:0x00-linear_algebra$ ./9-let_the_butcher_slice_it.py 
-The middle two rows of the matrix are:
-[[ 7  8  9 10 11 12]
- [13 14 15 16 17 18]]
-The middle two columns of the matrix are:
-[[ 3  4]
- [ 9 10]
- [15 16]
- [21 22]]
-The bottom-right, square, 3x3 matrix is:
-[[10 11 12]
- [16 17 18]
- [22 23 24]]
-alexa@ubuntu-xenial:0x00-linear_algebra$ wc -l 9-let_the_butcher_slice_it.py 
-10 9-let_the_butcher_slice_it.py
-alexa@ubuntu-xenial:0x00-linear_algebra$ 
