3e3a6fc5dc772bfffbfe22668cc1ec02972602fb
Britel Chaimaa <52888345+britel-chaimaa20@users.noreply.github.com>
2021-04-02 00:38:04 +0100

    Create 10-matisse.py

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''Calculates the derivate of a polynomial.
+'''
+
+
+def poly_derivative(poly):
+    '''Calculates the derivate of a polynomial.
+    Args:
+        poly: ist of coefficients representing a polynomial.
+    '''
+
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+    derivate = []
+    for idx, x in enumerate(poly):
+        if idx - 1 < 0:
+            pass
+        else:
+            derivate.insert(idx - 1, idx * x)
+    if len(derivate) == 0:
+        return [0]
+    else:
+        return derivate
